5669e594d46c29f5a3bb2b10e07851661e1feda2
Arctic7 <Arctic7@126.com>
2020-11-08 00:14:19 +0800

    增加读取xml下载路径的功能，写入到文件的功能

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 const https = require('https');
+const xmlParser = require('fast-xml-parser');
+const fs = require('fs');
 
-function getXml(){
+function getXml(serial){
     const req = https.request({
         hostname:'a0.ww.np.dl.playstation.net',
         port:443,
-        path:'/tpl/np/BCAS25003/BCAS25003-ver.xml',
+        path:`/tpl/np/${serial}/${serial}-ver.xml`,
         method:'GET',
         rejectUnauthorized:false
     },(resp)=>{
         resp.on('data',data=>{
-            console.log(data.toString());
+            let respContent = data.toString();
+            let respJsonObj = xmlParser.parse(respContent,{
+                ignoreAttributes:false,
+                attributeNamePrefix:''
+            });
+            let packages = respJsonObj.titlepatch.tag.package;
+            let downloadURLArray = [];
+            if(packages instanceof Array){
+                packages.forEach(ele=>{
+                    downloadURLArray.push(ele.url);
+                });
+
+            }else if(packages instanceof Object){
+                downloadURLArray.push(packages.url);
+            }
+            let content = downloadURLArray.join('\r\n');
+            fs.writeFile(__dirname+'/downloadurl.txt',content,{flag:'w'},(err)=>{
+                if(err){
+                    console.log('写入出错：'+err);
+                }
+            });
         });
     });
     req.on('error',err=>{
@@ -20,4 +42,4 @@ function getXml(){
 }
 
 
-getXml();
+getXml('BCAS25003');
